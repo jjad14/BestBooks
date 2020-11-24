efc1b3bfc09d7b77f0c20ffd6416f1aa5e9328aa
Dennis <dmarlo96@live.ca>
2020-11-24 01:15:13 -0500

    Cover Type Stored Procedures with Dapper

---
+++ BestBooks.DataAccess/Migrations/20201124050508_AddStoredProcedureForCoverType.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BestBooks.DataAccess.Migrations
+{
+    public partial class AddStoredProcedureForCoverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE usp_GetCoverTypes()
+                                    BEGIN
+	                                    SELECT * FROM   dbo.CoverTypes;
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE usp_GetCoverType(
+                                     IN Id int 
+                                    ) 
+                                    BEGIN 
+                                       SELECT * FROM   dbo.CoverTypes  WHERE  (Id = Id); 
+                                    END ");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE usp_UpdateCoverType(
+                                    IN Id int,
+                                    IN Name varchar(100)
+                                    )
+                                    BEGIN 
+                                     UPDATE dbo.CoverTypes
+                                     SET  Name = Name
+                                     WHERE  Id = Id;
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE usp_DeleteCoverType(
+                                    IN Id int
+                                    )
+                                    BEGIN 
+                                     DELETE FROM dbo.CoverTypes
+                                     WHERE  Id = Id;
+                                    END");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE usp_CreateCoverType(
+                                    IN Name varchar(100)
+                                    )
+                                    BEGIN 
+                                    INSERT INTO dbo.CoverTypes(Name)
+                                    VALUES (Name);
+                                    END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_GetCoverTypes");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_GetCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_UpdateCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_DeleteCoverType");
+            migrationBuilder.Sql(@"DROP PROCEDURE usp_CreateCoverType");
+        }
+    }
+}
---
+++ BestBooks/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BestBooks.DataAccess.Repository.IRepository;
+using BestBooks.Models;
+using BestBooks.Utility;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestBooks.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            CoverType coverType = new CoverType();
+
+            // create
+            if (id == null)
+            {
+                return View(coverType);
+            }
+
+            // stored procedure
+            // var parameter = new DynamicParameters();
+            // parameter.Add("@Id", id);
+            // coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+
+            // edit
+            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            if (coverType == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType coverType)
+        {
+            // if model validation is valid
+            if (ModelState.IsValid)
+            {
+                // stored procedure
+                // var parameter = new DynamicParameters();
+                // parameter.Add("@Name", coverType.Name);
+
+                if (coverType.Id == 0)
+                {
+                    // _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
+                    _unitOfWork.CoverType.Add(coverType);
+                }
+                else
+                {
+                    // parameter.Add("@Id", coverType.Id);
+                    // _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
+                    _unitOfWork.CoverType.Update(coverType);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index)); // or RedirectToAction("Index");
+            }
+
+            return View(coverType);
+
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // retrieve all the categories
+            var allObj = _unitOfWork.CoverType.GetAll();
+
+            // stored procedure
+            // var allObj2 = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null);
+
+            //  return in a json format
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            // stored procedure
+            // var parameter = new DynamicParameters();
+            // parameter.Add("@Id", id);
+            // var objFromDb2 = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+
+            var objFromDb = _unitOfWork.CoverType.Get(id);
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            // _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);
+
+            _unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Deleted Successfully" });
+        }
+
+        #endregion
+
+    }
+}
